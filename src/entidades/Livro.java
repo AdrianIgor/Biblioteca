72b288bf179bcbb5f162305ceed995f73cfff3e9
Adrian Igor <desenevil07@gmail.com>
2021-12-21 11:52:00 -0300

    Classes

---
+++ src/entidades/Livro.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author unity
+ */
+public class Livro {
+    
+    private String codigo;
+    private String nomeLivro;
+    private int paginas;
+    private String autor;
+    private double preco;
+    private String categoria;
+    private int edicao;
+
+    public Livro(String codigo, String nomeLivro, int paginas, String autor, double preco, String categoria, int edicao) {
+        this.codigo = codigo;
+        this.nomeLivro = nomeLivro;
+        this.paginas = paginas;
+        this.autor = autor;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.edicao = edicao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+    
+}
---
+++ src/entidades/Pessoa.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author unity
+ */
+public class Pessoa {
+    
+    private String nome;
+    private Long rg;
+    private Long cpf;
+    private String nascimento;
+    private double saldo;
+    private String senha;
+    
+    public Pessoa(){
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getRg() {
+        return rg;
+    }
+
+    public void setRg(Long rg) {
+        this.rg = rg;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
